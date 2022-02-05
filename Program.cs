c7a492b87a924f4c9b256cafd7b53b99b84453c4
Evgeniy Murzanaev <gramenbos@gmail.com>
2022-02-05 11:29:44 +0300

    Create PrintArray method

--- Program.cs
+++ Program.cs
@@ -6,3 +6,24 @@
         array[i] = rand.Next(1, 100);
     }
 }
+
+string PrintArray(int[] array)
+{
+    string stringArray = "[";
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i == array.Length - 1)
+        {
+            stringArray += $"{array[i]}";
+            break;
+        }
+        stringArray += ($"{array[i]}, ");
+    }
+    stringArray += "]";
+    return stringArray;
+}
+
+int[] workArray = new int[10];
+FillArray(workArray);
+string stringWorkArray = PrintArray(workArray);
+Console.WriteLine(stringWorkArray);
